afbf24f065df98cecb44a39ab55f29b32599357c
jscriptural <isongjohn014@gmail.com>
2025-10-25 19:23:42 +0100

    updated ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,11 @@
 import {Link} from 'react-router-dom';
 import Alert from "./Alert.jsx"
 import HeaderHome from "./headers/HeaderHome.jsx";
+import ThumbsUpIcon from "/3d-thumbs-up.png";
+import ThinkingIcon from "/cwok_casual_21.png";
+import MissingPieIcon from "/8046554.png";
+import UserCrowdIcon from "/crowd_16084887.png";
+
 
 export default function Home(){
   const msg = `This platform is freely accessible to schools and the public. Your generous support helps us provide high-quality educational resources at no cost. Please consider donating to:
@@ -10,16 +15,48 @@ Thank you for supporting our mission!
   return (<>
     <HeaderHome />
     <section id="home">
-      <p><strong>Yes!! It's absolutely  FREE</strong><br/>	
-    By simulating UTME with actual past 
-    questions, <strong>300plus</strong> aim to build aspirant's confidence and familiarity with the exam format.<br/>
-    <strong>300plus</strong> provide a free platform for aspirants to test the level of their readiness for the main exam.<br/> While we are facing some challenges like  sourcing for past questions and  maintaining the app, your support  can help offset the cost and keep this app free.
-    </p>
-      <Alert title="Support Us" message={msg} />
+      <div class="intro-container">
+	<figure class="intro">
+	  <img src={ThumbsUpIcon} alt="" />
+	  <figcaption><strong>Yes!! It&#39;s absolutely FREE</strong><br/>
+    B  y simulating UTME with actual past  questions, <strong>300plus</strong> aim to build aspirant&#39;s confidence and familiarity with the exam format.<br/> <strong>300plus</strong> provide a free platform for aspirants to test the level of their readiness for the main exam.
+	  </figcaption>
+	</figure>
+      </div>
+      <div className="why-us">
+	<h2> WHY CHOOSE US</h2>
+	<figure className="reason-1">
+	  <img src={ThinkingIcon} alt="" />
+	  <figcaption><strong>Yes!! It&#39;s absolutely FREE</strong><br/>
+      By simulating UTME with actual past  questions, <strong>300plus</strong> aim to build aspirant&#39;s confidence and familiarity with the exam format.<br/> <strong>300plus</strong> provide a free platform for aspirants to test the level of their readiness for the main exam.
+	  </figcaption>
+	</figure>
+	<figure className="reason-2">
+	  <img src={MissingPieIcon} alt="" />
+	  <figcaption><strong>Yes!! It&#39;s absolutely FREE</strong><br/>
+      By simulating UTME with actual past  questions, <strong>300plus</strong> aim to build aspirant&#39;s confidence and familiarity with the exam format.<br/> <strong>300plus</strong> provide a free platform for aspirants to test the level of their readiness for the main exam.
+	  </figcaption>
+	</figure>
+      </div>
+      <div className="outro">
+	<figure>
+	  <img src={UserCrowdIcon} alt="" />
+	  <figcaption>
+	    JOIN THE THOUSANDS OF USERS THAT TRUST US</figcaption>
+	</figure>
+
+	<div className="btn-container">
+	  <Link to="/auth/signup"><button>GET STARTED</button></Link>
+	</div>
+      </div>
+
+
+      
+    {/*<Alert title="Support Us" message={msg} />
       <ul  className="links">
 	<li><Link to="/auth/signup">Create an account</Link></li>
 	<li>Got an account?<Link to="/auth/login"> Login</Link></li>
-      </ul>
+      </ul>*/}
     </section>
     </>);
 }
